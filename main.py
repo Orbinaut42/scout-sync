93f05ebecb145875905d48a4424db4a5805320a5
Andreas Langbein <76401046+Orbinaut42@users.noreply.github.com>
2022-01-07 21:44:41 +0100

    add main.py and requirements.txt

---
+++ main.py
@@ -0,0 +1,16 @@
+import logging
+from flask import Flask, send_file
+from scout_sync import sync
+
+app = Flask('scout_sync')
+logging.getLogger('werkzeug').setLevel(logging.ERROR)
+
+
+@app.route('/')
+def root():
+    sync('schedule', 'calendar')
+    return send_file('scout_sync.log', mimetype='text/plain')
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='3000')
