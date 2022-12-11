a6250e28274b1353eac04ef0bc5e237c52a9cd95
Andreas Langbein <76401046+Orbinaut42@users.noreply.github.com>
2022-12-11 13:38:12 +0100

    process request arguments

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os
 import logging
-from flask import Flask, send_file
+from flask import Flask, send_file, request, abort
 from scout_sync import sync
 
 app = Flask('scout_sync')
@@ -9,8 +9,23 @@ logging.getLogger('werkzeug').setLevel(logging.ERROR)
 
 @app.route('/')
 def root():
-    sync('schedule', 'calendar')
-    return send_file('scout_sync.log', mimetype='text/plain')
+    parse_bool = lambda s: {'true': True, 'True': True, 'false': False, 'False': False}.get(s)
+
+    sync_request = request.args.get('sync', False, type=parse_bool)
+    if not isinstance(sync_request, bool):
+        abort(400, 'invalid "sync_request" argument')
+
+    if sync_request:
+        targets = ['calendar', 'schedule', 'table']
+        source = request.args.get('from')
+        dest = request.args.get('to')
+
+        if source not in targets or dest not in targets:
+            abort(400, 'invalid "from" or "to" argument')
+
+    answer = 'return table' if not sync_request else f"sync({source}, {dest})"
+    # sync('schedule', 'calendar')
+    return answer
 
 
 if __name__ == '__main__':
