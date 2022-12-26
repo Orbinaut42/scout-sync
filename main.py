8a18dbff751014996cfd3d481dde240a70a52c5e
Andreas Langbein <76401046+Orbinaut42@users.noreply.github.com>
2022-12-26 16:40:50 +0100

    log IP adresses of the requests

--- main.py
+++ main.py
@@ -5,14 +5,23 @@ from configparser import ConfigParser
 from flask import Flask, render_template, request, abort
 from scout_sync import sync, CalendarHandler
 
-app = Flask('scout_sync')
+_CONFIG = ConfigParser()
+_CONFIG.read('scout_sync.cfg', encoding='utf8')
+logging.basicConfig(
+    filename=_CONFIG.get('COMMON', 'log_file'),
+    format='%(asctime)s %(levelname)s: %(message)s',
+    datefmt='%Y-%m-%dT%H:%M:%S',
+    level=logging.INFO)
 logging.getLogger('werkzeug').setLevel(logging.ERROR)
+
+app = Flask('scout_sync')
 
 @app.route('/sync')
 def sync_():
     targets = ['calendar', 'schedule', 'table']
     source = request.args.get('from')
     dest = request.args.get('to')
+    logging.info(f'Sync request from {request.access_route[0]} ({source}, {dest})')
 
     if source not in targets or dest not in targets:
         abort(400, 'invalid "from" or "to" argument')
@@ -27,14 +36,12 @@ def sync_():
 
 @app.route('/')
 def root():
-    config = ConfigParser()
-    config.read('scout_sync.cfg', encoding='utf8')
-
-    calendar = CalendarHandler(config.get('CALENDAR', 'id'))
+    logging.info(f'Table request from {request.access_route[0]}')
+    calendar = CalendarHandler(_CONFIG.get('CALENDAR', 'id'))
     calendar.connect()
     events = calendar.list_events()
 
-    return render_template('game_list.html', events=events, today=arrow.now(config.get('COMMON', 'timezone')).date())
+    return render_template('game_list.html', events=events, today=arrow.now(_CONFIG.get('COMMON', 'timezone')).date())
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=os.environ.get('PORT', 3000))
