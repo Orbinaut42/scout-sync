88dccb6e73388fa1a4fd1e956734700468f1bc56
Andreas Langbein <76401046+Orbinaut42@users.noreply.github.com>
2024-04-15 17:00:40 +0200

    drop Google spreadsheet support

--- scout_sync/sync/sync.py
+++ scout_sync/sync/sync.py
@@ -3,8 +3,7 @@ import requests
 import arrow
 import json
 import time
-import pickle
-from .google_api import GoogleCalendarAPI, GoogleSheetsAPI
+from .google_api import GoogleCalendarAPI
 from ..config import config
 
 logging.basicConfig(
@@ -20,7 +19,7 @@ SIMULATE = config.getboolean('COMMON', 'simulate')
 
 
 class Event:
-    """Manages conversion between different event representation formats (JSON schedule, Google Calendar, Google Spreadsheet, ODS Table"""
+    """Manages conversion between different event representation formats (DBB schedule, Google Calendar, JSON)"""
 
     __emails = {k: v for k, v in config.items('EMAILS')}
     __names = {v: k for k, v in config.items('EMAILS')}
@@ -44,43 +43,6 @@ class Event:
         self.scouter3 = scouter3
         self.has_scouters = any([self.scouter1, self.scouter2, self.scouter3])
         self.schedule_info = schedule_info
-
-    @classmethod
-    def from_gsheets_table_row(cls, row, captions):
-        """Create an event from a Google Sheets row"""
-        row_vals = {cap: value for value, cap in zip(row, captions) if cap}
-        if row_vals.get('ID') in [None, '']:
-            logging.warning(f"Event without ID in table!")
-
-        if row_vals['Zeit'] == '':
-            row_vals['Zeit'] = 0
-
-        serial_date = None
-        if isinstance(row_vals['Datum'], (int, float)):
-            serial_date = row_vals['Datum']
-        else:
-            logging.warning(f"Invalid date in table: {row_vals['Datum']}")
-
-        if isinstance(row_vals['Zeit'], (int, float)):
-            if serial_date is not None:
-                serial_date += row_vals['Zeit']
-        else:
-            logging.warning(f"Invalid time in table: {row_vals['Zeit']}")
-
-        e = cls(
-            id = row_vals.get('ID'),
-            datetime = (GSheetsTableHandler.serial_to_datetime(
-                serial_date, TIMEZONE) if serial_date is not None else None),
-            location = row_vals.get('Halle') or None,
-            league = row_vals.get('Liga'),
-            opponent = row_vals.get('Gegner'),
-            scouter1 = row_vals.get('Scouter1'),
-            scouter2 = row_vals.get('Scouter2'),
-            scouter3 = row_vals.get('Scouter3'))
-        
-        e.has_scouters = True
-
-        return e
 
     @classmethod
     def from_calendar_event(cls, event):
@@ -123,7 +85,7 @@ class Event:
         return e
 
     @classmethod
-    def from_JSON_schedule(cls, event, league_name):
+    def from_DBB_schedule(cls, event, league_name):
         """Create an event from a JSON object (DBB schedule)"""
         try:
             datetime = arrow.get(f"{event['kickoffDate']}T{event['kickoffTime']}", tzinfo=TIMEZONE)
@@ -210,25 +172,6 @@ class Event:
         }
 
         return event
-
-    def as_gsheets_table_row(self, captions):
-        """create a representation of the event, that can be passed to the Google Sheets API"""
-
-        serial = GSheetsTableHandler.datetime_to_serial(self.datetime)
-        atts = {
-            'ID': self.id,
-            'Datum': int(serial),
-            'Zeit': (serial - int(serial)) or None,
-            'Halle': self.location,
-            'Liga': self.league,
-            'Gegner': self.opponent,
-            'Scouter1': self.scouter1,
-            'Scouter2': self.scouter2,
-            'Scouter3': self.scouter3
-        }
-
-        values = [atts[key] for key in captions]
-        return values
 
     def __str__(self):
         info_list = (
@@ -348,155 +291,6 @@ class CalendarHandler(GoogleCalendarAPI):
         return {ev['id']: Event.from_calendar_event(ev) for ev in ev_list}
 
 
-class GSheetsTableHandler(GoogleSheetsAPI):
-    """Manages the communication with the Google Calendar API"""
-
-    __captions_row = int(
-        config.get('GSHEETS_TABLE', 'captions_row', fallback=None) or 0)
-
-    def __init__(self, spredsheet_id, sheet_name, sheet_id):
-        super().__init__(spredsheet_id, sheet_name, sheet_id, TIMEZONE, SIMULATE)
-        self.__captions = []
-        self.__index = {}
-        self.__ids = []  # keeps track of which event is in which row of the sheet
-
-    def connect(self):
-        try:
-            self._connect_to_service()
-            for cell in self._get_range(self.__captions_row, dims=1):
-                self.__captions.extend(
-                    [cell]
-                    if cell != 'Scouter'
-                    else ['Scouter1', 'Scouter2', 'Scouter3'])
-
-            while self.__captions and not self.__captions[-1]:
-                del self.__captions[-1]
-
-            table_range = self._get_range(
-                self.__captions_row + 1,
-                9999,
-                dims=2)
-            
-            self.__ids = [None] * self.__captions_row
-            for id, row in enumerate(table_range):
-                if any(row):
-                    self.__index[id] = row
-                    self.__ids.append(id)
-
-                else:
-                    self.__ids.append(None)
-
-        except Exception as e:
-            logging.error(
-                f"Connection to Google spreadsheet with ID {self._resource_id} failed: {e}"
-            )
-            return False
-
-        logging.info(f"Connected to Google spreadsheet: {self._resource_id}")
-        return True
-
-    def add_events(self, events):
-        if not self._service or not events:
-            return
-
-        date_index = self.__captions.index('Datum')
-        time_index = self.__captions.index('Zeit')
-        insert_events = []
-        for ev in events:
-            for id, row in self.__index.items():
-                date_entry = row[date_index]
-                time_entry = row[time_index]
-                date = GoogleSheetsAPI.serial_to_datetime(
-                    (date_entry if isinstance(date_entry, (float, int)) else 0) +
-                    (time_entry if isinstance(time_entry, (float, int)) else 0),
-                    TIMEZONE)
-                
-                if date > (ev.datetime or arrow.Arrow(9999)):
-                    row_no = self.__ids.index(id)
-                    break
-
-            else:
-                row_no = len(self.__ids)
-
-            insert_events.append((row_no, ev.as_gsheets_table_row(self.__captions)))
-
-        # sort by date in reverse order, so later insertions will be in the correct row
-        insert_events.sort(
-            key=lambda e: e[1][date_index] + (e[1][time_index] or 0),
-            reverse=True)
-        
-        self._insert_rows(insert_events)
-        for row_no, event in insert_events:
-            new_id = max((self.__index.keys() or [-1])) + 1
-            self.__index[new_id] = event
-            self.__ids.insert(row_no, new_id)
-
-        for ev in events:
-            logging.info(
-                f"{'(SIMULATED) ' if SIMULATE else ''}Added event to table:\n\t\t{ev}"
-            )
-
-    def update_events(self, events):
-        if not self._service or not events:
-            return
-
-        update_events = [{
-            'id': id,
-            'row': self.__ids.index(id),
-            'old_event': Event.from_gsheets_table_row(self.__index[id], self.__captions),
-            'new_event': event}
-            for id, event in events.items()]
-
-        for event in update_events:
-            if not event['new_event'].has_scouters:
-                event['new_event'].scouter1 = event['old_event'].scouter1
-                event['new_event'].scouter2 = event['old_event'].scouter1
-                event['new_event'].scouter3 = event['old_event'].scouter1
-                event['new_event'].has_scouters = True
-
-        self._update_rows([
-            (ev['row'], ev['new_event'].as_gsheets_table_row(self.__captions))
-            for ev in update_events])
-
-        for ev in update_events:
-            self.__index[ev['id']] = ev['new_event'].as_gsheets_table_row(self.__captions)
-            logging.info(
-                f"{'(SIMULATED) ' if SIMULATE else ''}Updated event in table:\n\t-\t{ev['old_event']}\n\t+\t{ev['new_event']}"
-            )
-
-    def delete_events(self, event_ids):
-        if not self._service or not event_ids:
-            return
-
-        delete_events = [self.__ids.index(id) for id in event_ids]
-        # sort by date in reverse order, so later deletions will be in the correct row
-        delete_events.sort(reverse=True)
-        self._delete_rows(delete_events)
-
-        for id in event_ids:
-            event = Event.from_gsheets_table_row(self.__index[id],
-                                                 self.__captions)
-            del self.__index[id]
-            self.__ids.remove(id)
-            logging.info(
-                f"{'(SIMULATED) ' if SIMULATE else ''}Deleted event in table:\n\t\t{event}"
-            )
-
-    def list_events(self):
-        if not self._service:
-            return
-
-        try:
-            events = {
-                i: Event.from_gsheets_table_row(row, self.__captions)
-                for i, row in self.__index.items()}
-
-        except (TypeError, ValueError) as e:
-            logging.warning(f"Can not create event: {e}")
-
-        return events
-
-
 class ScheduleHandler:
     "manages downloads from the DBB game schedule database"
 
@@ -591,7 +385,7 @@ class ScheduleHandler:
             cancelled = any([match['abgesagt'], match['verzicht']])
 
             if not cancelled:
-                events.append(Event.from_JSON_schedule(match, league_name))
+                events.append(Event.from_DBB_schedule(match, league_name))
 
         return {i: event for i, event in enumerate(events)}
 
@@ -654,18 +448,12 @@ class ScheduleHandler:
 
 def sync(source, dest):
     """Start the syncronisation from 'source' to 'dest'
-    Allowed values for source are: schedule, calendar, table
-    Allowed values for destination are: calendar, table"""
+    Allowed values for source are: schedule, calendar
+    Allowed values for destination are: calendar"""
 
     logging.info(f"Starting sync from {source} to {dest}")
 
     start_time = time.time()
-
-    table_handler = (
-        GSheetsTableHandler,
-        config.get('GSHEETS_TABLE', 'id'),
-        config.get('GSHEETS_TABLE', 'sheet_name', fallback=None),
-        config.get('GSHEETS_TABLE', 'sheet_id', fallback=None))
 
     schedule_leagues = [
         config.getlist('SCHEDULE_LEAGUES', o)
@@ -673,7 +461,6 @@ def sync(source, dest):
     
     handler = {
         'calendar': (CalendarHandler, config.get('CALENDAR', 'id')),
-        'table': table_handler,
         'schedule': (ScheduleHandler, schedule_leagues)}
     
     source_hdl = handler[source][0](*handler[source][1:])
