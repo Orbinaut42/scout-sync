d92cc820b307b99af45121cf66aaf5cd1037ec75
Andreas Langbein <76401046+Orbinaut42@users.noreply.github.com>
2025-12-01 10:40:20 +0100

    make invitations work

--- scout_sync/sync/nextcloud.py
+++ scout_sync/sync/nextcloud.py
@@ -1,44 +1,23 @@
-import datetime
+from datetime import datetime, timezone, timedelta
 from caldav.davclient import get_davclient
-# from caldav.calendarobjectresource import Event
-# from caldav.lib.error import NotFoundError
-# from icalendar import vCalAddress, Event
-
-# a1 = vCalAddress('mailto:scouting.bbu@gmail.com')
-# a1.name = 'ScoutingBBU'
-
-# new_event.add_attendee('mailto:scouting.bbu@gmail.com')
-
-# print(new_event.to_ical().decode("utf-8").replace('\r\n', '\n').strip())
-
 
 with get_davclient() as client:
     my_principal = client.principal()
     my_calendar = my_principal.calendar()
     for ev in my_calendar.events():
-
-        if 'Event' in ev.component.get('summary') :
+        if 'Event' in ev.component.get('summary'):
             print(ev.data)
-
-    # print(ev.component.get('summary'))
-
-    # my_calendar.save_object(new_event)
+            ev.add_organizer()
+            ev.add_attendee(
+                ('Scouting BBU', 'scouting.bbu@gmail.com'),
+                cutype='INDIVIDUAL')
+            ev.save(increase_seqno=False)
+            print(ev.data)
+            ev.load()
+            ev.delete()
 
     # ev = my_calendar.save_event(
-    #     dtstart=datetime.datetime(2025, 12, 5, 13),
-    #     dtend=datetime.datetime(2025, 12, 5, 16),
-    #     summary="Event 5")
-
-    # my_calendar.save_with_invites(
-    #     ev.data,
-    #     attendees=['scouting.bbu@gmail.com'])
-
-    # ev.add_organizer()
-    # ev.add_attendee(
-    #     ('Scouting BBU', 'scouting.bbu@gmail.com'),
-    #     cutype='INDIVIDUAL')
-
-    # print(ev.data)
-    # ev.save()
-
-    # my_calendar.save()
+    #     dtstart=datetime(2025, 12, 2, 13, tzinfo=timezone(timedelta(hours=2))),
+    #     dtend=datetime(2025, 12, 2, 16, tzinfo=timezone(timedelta(hours=2))),
+    #     summary="Event caldav",
+    #     sequence=0)
